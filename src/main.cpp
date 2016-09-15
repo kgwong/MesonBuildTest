5da161e252c8da302374d12610b34a6d2517b02f
Kevin Wong <wong.kevin47@gmail.com>
2016-09-15 15:47:52 -0700

    Add useless_header

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "say.h"
 
+#include "useless_header.h"
+
 int main()
 {
     say();
+	do_nothing();
 	std::cin.ignore();
 	return 0;
 }
---
+++ src/useless_header.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void do_nothing()
+{
+	int i = 0;
+	i++;
+	return;
+}
